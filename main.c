cbd85445fe542319c82e0f4a35185f1b262add03
unknown <sweethomemrt@gmail.com>
2025-03-01 03:00:44 +0530

    optimized

--- main.c
+++ main.c
@@ -6,31 +6,24 @@ int main(void){
     scanf("%d",&n);
 
     for(int i=0;i<n+1;i++){
+        for (int k=n-i;k!=0;k--){
+            printf(" ");
+        }
         for(int j=0;j<i;j++){
-
-            if(j==0){
-                for (int k=n-i;k!=0;k--){
-                    printf(" ");
-                }
-            }
-
             printf("**");
         }
-        if(i!=n){
-            printf("\n");
-        }
+        printf("\n");
+        
     }
     for(int i=0;i<n+1;i++){
-      for(int j=0;j<i;j++){
-        printf(" ");
-        if(j==i-1){
-            for(int k=n-i;k!=0;k--){
-                printf("**");
-            }
+        for(int j=0;j<i;j++){
+            printf(" ");  
         }
-      }
-      printf("\n");
+        for(int k=n-i;k!=0;k--){
+            printf("**");
+        }
+        printf("\n");
     }
 
-    
+   return 0; 
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+  // for(int i=0;i<n+1;i++){
+    //   for(int j=0;j<i;j++){
+    //     printf(" ");
+       
+    //   }
+    //   for(int k=n-i;k!=0;k--){
+    //     printf("**");
+    // }
+    //   printf("\n");
+    // }
