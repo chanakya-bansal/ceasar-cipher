a8f8dd29ec8142f8cac32e84f1f8e92bd2c39011
unknown <sweethomemrt@gmail.com>
2025-03-01 02:48:55 +0530

    diamond

--- main.c
+++ main.c
@@ -2,18 +2,35 @@
 
 int main(void){
     int n;
+    printf("input -->");
     scanf("%d",&n);
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i+1;j++){
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(int i=n;i!=0;i--){
+    for(int i=0;i<n+1;i++){
         for(int j=0;j<i;j++){
-            printf("*");
+
+            if(j==0){
+                for (int k=n-i;k!=0;k--){
+                    printf(" ");
+                }
+            }
+
+            printf("**");
         }
-        printf("\n");
+        if(i!=n){
+            printf("\n");
+        }
     }
+    for(int i=0;i<n+1;i++){
+      for(int j=0;j<i;j++){
+        printf(" ");
+        if(j==i-1){
+            for(int k=n-i;k!=0;k--){
+                printf("**");
+            }
+        }
+      }
+      printf("\n");
+    }
+
+    
 }
