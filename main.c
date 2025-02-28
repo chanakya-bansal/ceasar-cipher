aac2252a58d1a94593b4448a439754ce5934c6c6
unknown <sweethomemrt@gmail.com>
2025-03-01 02:17:14 +0530

    2nd commit

--- main.c
+++ main.c
@@ -10,4 +10,10 @@ int main(void){
         }
         printf("\n");
     }
+    for(int i=n;i!=0;i--){
+        for(int j=0;j<i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 }
